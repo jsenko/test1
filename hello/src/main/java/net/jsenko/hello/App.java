ba6af49c8ff31dba3111cbd3d52da4036ff9f760
Jakub Senko <jsenko@redhat.com>
2013-12-18 18:55:11 +0100

    Created the app using mvn archetype.

---
+++ hello/src/main/java/net/jsenko/hello/App.java
@@ -0,0 +1,13 @@
+package net.jsenko.hello;
+
+/**
+ * Hello!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ hello/src/test/java/net/jsenko/hello/AppTest.java
@@ -0,0 +1,28 @@
+package net.jsenko.hello;
+
+import junit.framework.*;
+
+/**
+ * Unit test for the App.
+ */
+public class AppTest extends TestCase
+{
+
+    public AppTest(String testName)
+    {
+        super(testName);
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue(true);
+    }
+}
